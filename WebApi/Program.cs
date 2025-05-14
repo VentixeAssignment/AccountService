5e13f17e6a8079c638f417b983f756701a841d2e
Åsa Johansson <asa.j.1989@gmail.com>
2025-05-14 14:23:22 +0200

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+builder.Services.AddScoped<IAuthService, IAuthService>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequiredLength = 8;
+    x.Password.RequireDigit = true;
+    x.Password.RequireUppercase = true;
+    x.Password.RequireLowercase = true;
+}).AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
