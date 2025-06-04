e4887ab5545dbed99fc09cca3b2d667cd743ee51
Åsa Johansson <asa.j.1989@gmail.com>
2025-06-04 20:05:33 +0200

    update reading origins in program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,20 +10,22 @@ using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
 var grpcUri = builder.Configuration["GrpcUri"];
 
+var allowedOrigins = builder.Configuration["AllowedOrigins"];
+var originArray = allowedOrigins?.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-if (allowedOrigins == null || allowedOrigins.Length == 0)
+if (originArray == null || originArray.Length == 0)
 {
     throw new Exception($"Appsettings not loaded correctly. {allowedOrigins}");
 }
+
 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins(allowedOrigins)
+        policy.WithOrigins(originArray)
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
